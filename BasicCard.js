df861529358edb8a30d2b7fb6abde1f48eb2f2b8
Danae Boyd <danae.boyd@gmail.com>
2017-07-01 01:56:36 -0700

    updated

--- BasicCard.js
+++ BasicCard.js
@@ -1,13 +1,21 @@
-function Card(front, back){
-    this.frontVal = front;
-    this.backVal = back;
-    this.display = function(side){
-        if( side === 0 ){
-            return this.frontVal;
-        }else{
-            return this.backVal;
-        }
-    };
+var Card = function(front, back) {
+    this.front = front;
+    this.back = back;
+
 }
+
+Card.prototype.printCard = function() {
+    console.log('Front: ' + this.front + ', ' + 'Back: ' + this.back);
+};
+
+Card.prototype.printFront = function() {
+    console.log(this.front);
+
+};
+
+
+Card.prototype.printAnswer = function() {
+    console.log('Answer: ' + this.back + '.');
+};
 
 module.exports = BasicCard;
